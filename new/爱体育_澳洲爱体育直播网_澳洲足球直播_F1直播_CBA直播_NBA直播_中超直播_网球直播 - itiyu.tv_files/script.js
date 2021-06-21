3809e8b27b9e30bd01baa7cae314850565b7842c
Stevvvvvi <lly363893886@gmail.com>
2021-06-21 15:43:43 +1000

    add href link to each video

--- new/爱体育_澳洲爱体育直播网_澳洲足球直播_F1直播_CBA直播_NBA直播_中超直播_网球直播 - itiyu.tv_files/script.js
+++ new/爱体育_澳洲爱体育直播网_澳洲足球直播_F1直播_CBA直播_NBA直播_中超直播_网球直播 - itiyu.tv_files/script.js
@@ -30,36 +30,42 @@ const videoData=[
     name:"2020欧洲杯",
     match:"意大利 VS 瑞士",
     state:"全场录像",
+    href:"http://www.iyingshi9.tv/play/83396-0-1.html",
   },
   {
     src:"./爱体育_澳洲爱体育直播网_澳洲足球直播_F1直播_CBA直播_NBA直播_中超直播_网球直播 - itiyu.tv_files/60caad105e475.jpg",
     name:"2020欧洲杯",
     match:"土耳其 VS 威尔士",
     state:"全场录像",
+    href: "http://www.iyingshi9.tv/play/83395-0-1.html",
   },
   {
     src:"./爱体育_澳洲爱体育直播网_澳洲足球直播_F1直播_CBA直播_NBA直播_中超直播_网球直播 - itiyu.tv_files/60caac6c8edd6.jpg",
     name:"2020欧洲杯",
     match:"芬兰 VS 俄罗斯",
     state:"全场录像",
+    href:"http://www.iyingshi9.tv/play/83394-0-1.html",
   },
   {
     src:"./爱体育_澳洲爱体育直播网_澳洲足球直播_F1直播_CBA直播_NBA直播_中超直播_网球直播 - itiyu.tv_files/60caac11e2a2b.jpg",
     name:"2020欧洲杯",
     match:"意大利 VS 瑞士",
     state:"全场录像",
+    href:"http://www.iyingshi9.tv/play/83393-0-1.html",
   },
   {
     src:"./爱体育_澳洲爱体育直播网_澳洲足球直播_F1直播_CBA直播_NBA直播_中超直播_网球直播 - itiyu.tv_files/60caa64226de8.jpg",
     name:"2020欧洲杯",
     match:"土耳其 VS 威尔士",
     state:"全场录像",
+    href:"http://www.iyingshi9.tv/play/83392-0-1.html",
   },
   {
     src:"./爱体育_澳洲爱体育直播网_澳洲足球直播_F1直播_CBA直播_NBA直播_中超直播_网球直播 - itiyu.tv_files/60caa4b8cb1c5.jpg",
     name:"2020欧洲杯",
     match:"芬兰 VS 俄罗斯",
     state:"全场录像",
+    href:"http://www.iyingshi9.tv/play/83391-0-1.html",
   },
 ];
 
@@ -68,6 +74,7 @@ function changeMainVideo(v){
   $('#video_class_name').html(`${videoData[v].name}`);
   $('#video_match_name').html(`${videoData[v].match}`);
   $('#video_state').html(`${videoData[v].state}`)
+  $('#video_main_href').attr("href",`${videoData[v].href}`)
 }
 $('input[type=radio][name="radio-btn"]').change(function() {
   changeMainVideo(this.value)
