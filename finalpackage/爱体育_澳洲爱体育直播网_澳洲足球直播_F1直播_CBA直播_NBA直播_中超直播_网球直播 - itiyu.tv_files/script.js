a963cd38e49f22295765a5ca01d3f88aea1017c4
Stevvvvvi <lly363893886@gmail.com>
2021-07-01 15:51:17 +1000

    final package

---
+++ finalpackage/爱体育_澳洲爱体育直播网_澳洲足球直播_F1直播_CBA直播_NBA直播_中超直播_网球直播 - itiyu.tv_files/script.js
@@ -0,0 +1,106 @@
+// header 的 scroll 特效
+function headerScrollAnmiation(){
+    var scrollPosition=0;
+    $(window).on('scroll', function(){
+      $('.header_content').toggleClass('hide', $(window).scrollTop()>scrollPosition);
+      scrollPosition=$(window).scrollTop();
+    })
+}
+headerScrollAnmiation();
+
+// hamburger bar
+function toggleMenuShow(x) {
+  if (x.classList.contains("change")){
+    x.classList.remove("change")
+  }else{
+    x.classList.add("change")
+  }
+}
+
+// head slide automated change and click event
+var slidePlaceCounter=1;
+setInterval(function(){
+  let willChangeSlide=document.getElementById("slide_text"+slidePlaceCounter)
+  toggleSlide(willChangeSlide);
+  slidePlaceCounter++;
+  if( slidePlaceCounter> 3){
+    slidePlaceCounter=1;
+  }
+},5000);
+
+
+function toggleSlide(x){
+  // console.log(x.id.match(/\d+/g)[0]);
+  $('.slide').removeClass('active_slide');
+  $(`#img_container${x.id.match(/\d+/g)[0]}`).addClass('active_slide');
+  $('.slide_text').removeClass('active_text');
+  x.classList.add("active_text");
+  slidePlaceCounter=x.id.match(/\d+/g)[0];
+}
+
+// broadcast scroll button logic
+$.fn.isScrollable = function () {
+  return this[0].scrollWidth > this[0].clientWidth;
+};
+const cards=$(".broadcast_cards");
+const sliderLeft=$('#slider_left');
+const sliderRight=$('#slider_right');
+
+function displaySlider(){
+  if (cards.isScrollable()){
+    if (cards.scrollLeft()==0){
+      sliderLeft.hide();
+      sliderRight.show();
+    }else if (cards.scrollLeft() == (cards[0].scrollWidth - cards[0].clientWidth)){
+      sliderRight.hide();
+      sliderLeft.show();
+    }else{
+      sliderLeft.show();
+      sliderRight.show();
+    }
+  }else{
+    sliderLeft.hide();
+    sliderRight.hide();
+  };
+}
+displaySlider();
+$(window).resize(()=>displaySlider());
+
+sliderLeft.click(()=>{
+  var postion = cards.scrollLeft()
+  if (postion<260){
+    cards.animate({scrollLeft:'0'},300);
+   
+  }else{
+    cards.animate({scrollLeft:'-=260'},300);
+  }
+})
+
+sliderRight.click(()=>{
+  var postion = cards.scrollLeft();
+  if (postion + cards[0].clientWidth + 260 > cards[0].scrollWidth){
+    cards.animate({scrollLeft: cards[0].scrollWidth },300);
+  }else{
+    cards.animate({scrollLeft:'+=260'},300);
+  }
+})
+
+cards.scroll(()=>{
+  displaySlider();
+})
+
+
+// logic for menu placement
+$('.weeks').click(function(){
+  $('.weeks').removeClass('active');
+  $(this).addClass('active');
+  // $('html, body').animate({
+  //           scrollTop: $(this.id).position().top
+  //       }, 400);
+  //       return false;
+  $('.weekly_wrapper').addClass("hide");
+  // $(this.id).fadeIn();
+  $(this.id).removeClass("hide");
+  displaySlider();
+})
+
